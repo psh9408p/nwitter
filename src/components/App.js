780d6aeda955ece1e4f1fd5f86bd11a2d5a17877
박성현 <parksh@bagseonghyeon-ui-MacBookPro.local>
2020-11-19 17:21:55 +0900

    add login

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
+import React, { useState, useEffect } from "react";
 import AppRouter from "./Router";
-
+import { authService } from "../fbase";
 function App() {
-  return <AppRouter />;
+  const [Init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
+  setInterval(() => {
+    console.log(authService.currentUser);
+  }, 2000);
+  return <>{Init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initalizing"}</>;
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { authService } from "../fbase";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, seterror] = useState("");
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+    } catch (error) {
+      seterror(error.message);
+    }
+  };
+
+  const toggleAccount = () => {
+    setNewAccount((prev) => !prev);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Login"}
+          required
+        />
+        {error}
+      </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign in" : "Create Account"}
+      </span>
+      <div>
+        <button>continue google</button>
+        <button>continue github</button>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
